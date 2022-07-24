e50efae02005690197fbc9ba411ccf958736e6f8
AlbertZhang <itsalbertzhang@gmail.com>
2022-07-24 12:38:41 +0800

    2207241238

--- src/get_dat.py
+++ src/get_dat.py
@@ -1,15 +1,13 @@
+from genericpath import isdir
 from pathlib import Path
-import os
 import zipfile
 import shutil
 
 
 def unzip_patchfile():
     patchdir = Path('./patch')
-    ls = os.listdir(patchdir)
-    for filename in ls:
-        f = patchdir / filename
-        if not '.gitkeep' == filename and not f.is_dir() and f.exists():
+    for f in patchdir.iterdir():
+        if f.exists() and not f.is_dir() and not '.gitkeep' == f.name:
             unzipdir = f.stem
             key = '-to-1.0.0.'
             p = unzipdir.find(key)
@@ -20,32 +18,23 @@ def unzip_patchfile():
 
 
 def movedat():
-    datdir = Path('./bin/dat')
-    patchdir = Path('./patch')
-    ls_patchdir = os.listdir(patchdir)
-    # ls_patchdir: ['1000-1001', '1001-1002']
-    for patchname in ls_patchdir:
-        d = patchdir / patchname
-        # d: ./patch/1000-1001
-        if d.is_dir():
-            d = d / 'updatepack' / 'pakv4'
-            # d: ./patch/1000-1001/updatepack/pakv4
-            d = d / os.listdir(d)[0]
-            ls_d = os.listdir(d)
-            # ls_d: ['0', '000', '001']
-            for id in ls_d:
-                iddir = d / id
-                # iddir: ./patch/1000-1001/updatepack/pakv4/0
-                if iddir.is_dir():
-                    ls_dat = os.listdir(iddir)
-                    # ls_dat: ['000.dat', '000.idx', '001.dat', '001.idx', 'Apatch.list'...]
-                    for filename in ls_dat:
-                        file_moved = iddir / filename
-                        # file_moved: ./patch/1000-1001/updatepack/pakv4/0/000.dat
-                        # 或: ./patch/1000-1001/updatepack/pakv4/0/000.idx
-                        if file_moved.exists() and file_moved.suffix == '.dat':
-                            shutil.move(file_moved, datdir / (patchname + '_' + id + '_' + file_moved.name))
-                            shutil.move(iddir / (file_moved.stem + '.idx'), datdir / (patchname + '_' + id + '_' + file_moved.stem + '.idx'))
+    path_dat = Path('./bin/dat')
+    path_patch = Path('./patch')
+    for path_patch_version in path_patch.iterdir():
+        # patch_version: ./patch/1000-1001 等
+        if path_patch_version.is_dir():
+            path_id_parent = path_patch_version / 'updatepack' / 'pakv4'
+            # path_id_parent: ./patch/1000-1001/updatepack/pakv4
+            for path_id_parent in path_id_parent.iterdir():
+                # path_id_parent: ./patch/1000-1001/updatepack/pakv4/1000-1001(1.0.0.1000-1.0.0.1001).update (实际上只有一个目录)
+                for path_id in path_id_parent.iterdir():
+                    # path_id: ./patch/1000-1001/updatepack/pakv4/1000-1001(1.0.0.1000-1.0.0.1001).update/0 等
+                    if path_id.is_dir():
+                        for path_datfile in path_id.iterdir():
+                            # path_datfile: ./patch/1000-1001/updatepack/pakv4/1000-1001(1.0.0.1000-1.0.0.1001).update/0/000.dat 等
+                            if path_datfile.exists() and path_datfile.suffix == '.dat':
+                                shutil.move(path_datfile, path_dat / (path_patch_version.name + '_' + path_id.name + '_' + path_datfile.name))
+                                shutil.move(path_id / (path_datfile.stem + '.idx'), path_dat / (path_patch_version.name + '_' + path_id.name + '_' + path_datfile.stem + '.idx'))
 
 
 if '__main__' == __name__:
